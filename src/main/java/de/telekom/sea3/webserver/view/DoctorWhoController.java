980a85095177a063719ab6d1b0d002e034cd520f
juliShmi <juliashmitko@gmail.com>
2021-06-18 14:44:14 +0300

    DoctorWho added

---
+++ src/main/java/de/telekom/sea3/webserver/view/DoctorWhoController.java
@@ -0,0 +1,43 @@
+package de.telekom.sea3.webserver.view;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DoctorWhoController {
+
+	private static final String HTMLTEMPLATE ="<!DOCTYPE html>" + "<html lang='de'><head> <meta charset='utf-8'>"
+			+ "<meta name='viewport' content='width=device-width, initial-scale=1.0'> " + "<title>Titel</title> "
+			
+			+ "</head> <body><ul>"			+ "				    <li><a href='https://en.wikipedia.org/wiki/Ninth_Doctor'>Ninth Doctor</a></li>"
+			+ "				    <li><a href='https://en.wikipedia.org/wiki/Tenth_Doctor'>Tenth Doctor</a></li>"
+			+ "				    <li><a href='https://en.wikipedia.org/wiki/Eleventh_Doctor'>Eleventh Doctor</a></li>"
+			+ "				    <li><a href='https://en.wikipedia.org/wiki/Twelfth_Doctor'>Twelfth Doctor</a></li>"
+			+ "				  </ul></body>" + "</html>";
+
+	@RequestMapping("/doctor/9")
+	public String returnNinth() {
+		return "Greetings from <a href='https://en.wikipedia.org/wiki/Ninth_Doctor'>Ninth Doctor!</a>";
+	}
+
+	@RequestMapping("/doctor/10")
+	public String returnTenth() {
+		return "Greetings from <a href='https://en.wikipedia.org/wiki/Tenth_Doctor'>Tenth Doctor!</a>";
+	}
+
+	@RequestMapping("/doctor/11")
+	public String returnEleventh() {
+		return "Greetings from <a href='https://en.wikipedia.org/wiki/Eleventh_Doctor'>Eleventh Doctor!</a>";
+	}
+
+	@RequestMapping("/doctor/12")
+	public String returnTwelfth() {
+		return "Greetings from <a href='https://en.wikipedia.org/wiki/Twelfth_Doctor'>Twelfth Doctor!</a>";
+	}
+
+	@RequestMapping("/")
+	public String returnLinkList() {
+		return HTMLTEMPLATE;
+	}
+
+}
